dc185a53ecae91e579551188c26aec07724839c3
mmitch2958 <mikescottmitchell@outlook.com>
2020-10-01 14:03:37 -0400

    test

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ const server = require('./server.js')
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log('server running'))
+server.get('/', (req, res) => {
+    res.status(200).json({ hello: "hello world", environment: NODE_ENV })
+})
 
  //add start script to package.json 
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const server = express();
 
-
+server.get('/', (req, res) => {
+    res.status(200).json({
+        hello: "hello world",
+        environment: process.env.NODE_ENV,
+    })
+})
 server.get('/', (req, res) => {
     res.status(200).json({ hello: 'hello' });
 });
